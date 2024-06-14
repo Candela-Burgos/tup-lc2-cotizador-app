20ec579a179310367dbc34c554f699418813b3d8
Candela <candelaburgos10@yahoo.com>
2024-06-14 11:56:12 -0300

    Creamos carpetas de js

---
+++ js/common.js
@@ -0,0 +1,13 @@
+/* --------------- CARGARGAMOS LA API --------------- */
+
+/* https://dolarapi.com/docs/argentina/ */
+
+const dolarApi = async () => {
+  try {
+    const respuesta = await fetch("https://dolarapi.com/v1/dolares");
+    console.log(respuesta);
+  } catch (error) {
+    console.log(error);
+  }
+};
+dolarApi();

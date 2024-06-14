4e4e12a15172b936a51859ea691a2e43bfa6d8ff
Candela <candelaburgos10@yahoo.com>
2024-06-14 12:44:39 -0300

    Tomamos los valores de la api para la pizarra

--- js/common.js
+++ js/common.js
@@ -2,14 +2,32 @@
 
 /* https://dolarapi.com/docs/argentina/ */
 
+const filtrar_pizarra = document.getElementById("filtrarPizarra");
+const selector_moneda = document.getElementById("selecMoneda");
+
 const dolarApi = async () => {
   try {
-    const respuesta = await fetch("https://dolarapi.com/v1/dolares");
-    console.log(respuesta);
+    const respuesta = await fetch(
+      `https://dolarapi.com/v1/${selector_moneda.value}`
+    );
+    const data = await respuesta.json();
+    for (const dato in data) {
+      if (Object.hasOwnProperty.call(data, dato)) {
+        const element = data[dato];
+        console.log(element);
+        /* console.log(element.moneda);
+        console.log(element.casa);
+        console.log(element.nombre);
+        console.log(element.compra);
+        console.log(element.venta);
+        console.log(element.fechaActualizacion); */
+      }
+    }
   } catch (error) {
     console.log(error);
   }
 };
-dolarApi();
 
-// HOLA SOY UN COMENTARIO JAJA
+/* dolarApi(); */
+
+filtrar_pizarra.addEventListener("click", dolarApi);

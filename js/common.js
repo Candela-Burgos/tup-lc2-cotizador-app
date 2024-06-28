7ee76d86d52efdf397511041c6b922c4d0e3acc0
ValeLan <vale.lanf@gmail.com>
2024-06-28 12:27:15 -0300

    Corregimos la llamda a la API y empezamos a llenar las tarjetas

--- js/common.js
+++ js/common.js
@@ -2,38 +2,118 @@
 
 /* https://dolarapi.com/docs/argentina/ */
 
+
 const selector_moneda = document.getElementById("selecMoneda");
+const filtrar_pizarra = document.getElementById("filtrarPizarra");
+const pizzarra_cotz = document.getElementById("cotizaciones");
+const filtroPizarra = () => {
 
-const obtenerYAlmacenarDatosApi = async (url) => {
-  try {
-    const respuesta = await fetch(url);
-    if (!respuesta.ok) {
-      throw new Error(`Error en la petición: ${respuesta.statusText}`);
-    }
-    const datos = await respuesta.json();
+  if (selector_moneda.value == "todas"){
 
-    // Almacenar datos en localStorage con una clave específica
-    localStorage.setItem('datosMoneda', JSON.stringify(datos));
-    console.log('Datos almacenados en localStorage:', datos);
-  } catch (error) {
-    console.error('Error al obtener datos de la API:', error);
   }
-};
+}
 
-const iniciarProceso = () => {
-  const url = `https://dolarapi.com/v1/${selector_moneda.value}`; // URL de la API
+const procesoIniciado = async() => {
 
-  // Llamar a la función cuando la página se carga
-  obtenerYAlmacenarDatosApi(url);
+  const monedas = {
+    USD : "https://dolarapi.com/v1/dolares",
+    cotizaciones: "https://dolarapi.com/v1/cotizaciones"
 
-  // Configurar el intervalo para llamar a la función cada 5 minutos
-  setInterval(() => {
-    obtenerYAlmacenarDatosApi(url);
-  }, 300000); // 300,000 milisegundos = 5 minutos
-};
+  }
 
-// Evento que inicia el proceso cuando el cuerpo de la página se carga
-document.body.onload = iniciarProceso;
+    try {
+      const respuestaUSD = await fetch(monedas.USD)
+      const respuestaCotz = await fetch(monedas.cotizaciones)
+      if (respuestaUSD.ok){
+        const dataUSD = await respuestaUSD.json()
+        for (let i = 0; i < dataUSD.length; i++) {
+          console.log(dataUSD[i].casa)
+          
+          const cotz = 
+          `
+          <div class="cotizacion">
+                <div class="moneda">
+                  <p>${dataUSD.casa}</p>
+                  <div class="compraventa">
+                    <div class="compra">
+                      <h5>COMPRA</h5>
+                      <p>${dataUSD.compra}</p>
+                    </div>
+                    <div class="venta">
+                      <h5>VENTA</h5>
+                      <p>${dataUSD.venta}</p>
+                    </div>
+                  </div>
+                </div>
+                <button type="button" class="botonFavorito">
+                  <i class="fa-solid fa-star pintada"></i>
+                </button>
+              </div>
+          `
+          pizzarra_cotz.appendChild(cotz)
+          
+          console.log(dataUSD[i].venta)
+       }
+      }
+      if (respuestaCotz.ok){
+        const dataCotz = await respuestaCotz.json()
+        for (let i = 0; i < dataCotz.length; i++) {
+          console.log(dataCotz[i].compra)
+          console.log(dataCotz[i].venta)
+       }
+      }
+
+    }
+    catch(error){
+      console.log("Hubo un error.")
+    }
+
+    
+}
+procesoIniciado()
+
+setInterval(function(){
+  procesoIniciado()
+}, 50000);
+
+
+
+
+
+
+
+// const selector_moneda = document.getElementById("selecMoneda");
+
+// const obtenerYAlmacenarDatosApi = async (url) => {
+//   try {
+//     const respuesta = await fetch(url);
+//     if (!respuesta.ok) {
+//       throw new Error(`Error en la petición: ${respuesta.statusText}`);
+//     }
+//     const datos = await respuesta.json();
+
+//     // Almacenar datos en localStorage con una clave específica
+//     localStorage.setItem('datosMoneda', JSON.stringify(datos));
+//     console.log('Datos almacenados en localStorage:', datos);
+//   } catch (error) {
+//     console.error('Error al obtener datos de la API:', error);
+//   }
+// };
+
+// const iniciarProceso = () => {
+//   const url = `https://dolarapi.com/v1/${selector_moneda.value}`; // URL de la API
+
+//   // Llamar a la función cuando la página se carga
+//   obtenerYAlmacenarDatosApi(url);
+
+//   // Configurar el intervalo para llamar a la función cada 5 minutos
+//   setInterval(() => {
+//     obtenerYAlmacenarDatosApi(url);
+//   }, 300000); // 300,000 milisegundos = 5 minutos
+// };
+
+// // Evento que inicia el proceso cuando el cuerpo de la página se carga
+// document.body.onload = iniciarProceso;
 
 
 
